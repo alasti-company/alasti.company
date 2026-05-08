d373cdb278824937a8b724478a5ce5905392bf55
Kourosh Alasti <coding@kouroshalasti.com>
2026-05-08 11:16:03 -0700

    feat: add tracking for products

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
+import { configure } from "onedollarstats";
+
 import "./style.css";
+
+configure({
+  hostname: "alasti.company",
+  devmode: false,
+});
 
 if (window.location.pathname === "/github" || window.location.pathname === "/github/") {
   window.location.replace("https://github.com/alasti-company");
@@ -13,16 +20,16 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     LOCATION: SAN FRANCISCO, CA
 
     PRODUCTS:
-      - <a target="_blank" href="https://alasti.email">https://alasti.email</a></li>
-      - <a target="_blank" href="https://usealasti.com">https://usealasti.com</a></li>
-      - <a target="_blank" href="https://wikiflai.com">https://wikiflai.com</a></li>
-      - <a target="_blank" href="https://unishopapp.com">https://unishopapp.com</a></li>
-      - <a target="_blank" href="https://futdrafts.com">https://futdrafts.com</a></li>
-      - <a target="_blank" href="https://lyraly.com">https://lyraly.com</a></li>
-      - <a target="_blank" href="https://healthzkit.dev">https://healthzkit.dev</a></li>
-      - <a target="_blank" href="https://releasely.dev">https://releasely.dev</a></li>
-      - <a target="_blank" href="https://baselines.dev">https://baselines.dev</a></li>
-      - <a target="_blank" href="https://gitless.dev">https://gitless.dev</a></li>
+      - <a data-s-event="click:alasti.email" target="_blank" href="https://alasti.email">https://alasti.email</a></li>
+      - <a data-s-event="click:usealasti.com" target="_blank" href="https://usealasti.com">https://usealasti.com</a></li>
+      - <a data-s-event="click:wikiflai.com" target="_blank" href="https://wikiflai.com">https://wikiflai.com</a></li>
+      - <a data-s-event="click:unishopapp.com" target="_blank" href="https://unishopapp.com">https://unishopapp.com</a></li>
+      - <a data-s-event="click:futdrafts.com" target="_blank" href="https://futdrafts.com">https://futdrafts.com</a></li>
+      - <a data-s-event="click:lyraly.com" target="_blank" href="https://lyraly.com">https://lyraly.com</a></li>
+      - <a data-s-event="click:healthzkit.dev" target="_blank" href="https://healthzkit.dev">https://healthzkit.dev</a></li>
+      - <a data-s-event="click:releasely.dev" target="_blank" href="https://releasely.dev">https://releasely.dev</a></li>
+      - <a data-s-event="click:baselines.dev" target="_blank" href="https://baselines.dev">https://baselines.dev</a></li>
+      - <a data-s-event="click:gitless.dev" target="_blank" href="https://gitless.dev">https://gitless.dev</a></li>
 
     © ${new Date().getFullYear()} THE ALASTI COMPANY 
   </code>
